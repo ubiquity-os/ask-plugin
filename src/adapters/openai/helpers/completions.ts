258c0779e903a3a0996f978529ffca5d2a2cec89
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2024-10-25 00:48:13 +0100

    chore: tool handling

--- src/adapters/openai/helpers/completions.ts
+++ src/adapters/openai/helpers/completions.ts
@@ -1,16 +1,12 @@
 import OpenAI from "openai";
 import { Context } from "../../../types";
 import { SuperOpenAi } from "./openai";
-const MAX_TOKENS = 7000;
-
-export interface ResponseFromLlm {
-  answer: string;
-  tokenUsage: {
-    input: number;
-    output: number;
-    total: number;
-  };
-}
+import { logger } from "../../../helpers/errors";
+import { appendToConversation } from "./append-to-base-chat-history";
+import { getAnswerAndTokenUsage } from "./get-answer-and-token-usage";
+import { CreationParams, ResponseFromLlm, ToolCallResponse } from "../types";
+import { MAX_COMPLETION_TOKENS } from "../constants";
+// import { LLM_TOOLS } from "./llm-tools";
 
 export class Completions extends SuperOpenAi {
   protected context: Context;
@@ -20,66 +16,18 @@ export class Completions extends SuperOpenAi {
     this.context = context;
   }
 
-  private _createSystemMessage(systemMessage: string, additionalContext: string[], localContext: string[], groundTruths: string[], botName: string) {
-    // safer to use array join than string concatenation
-    const parts = [
-      "You Must obey the following ground truths: [",
-      groundTruths.join(":"),
-      "]\n",
-      systemMessage,
-      "Your name is : ",
-      botName,
-      "\n",
-      "Primary Context: ",
-      additionalContext.join("\n"),
-      "\nLocal Context: ",
-      localContext.join("\n"),
-    ];
-
-    return parts.join("\n");
-  }
-
-  async createCompletion({
-    systemMessage,
-    prompt,
-    model = "o1-mini",
-    additionalContext,
-    localContext,
-    groundTruths,
-    botName,
-  }: {
-    systemMessage: string;
-    prompt: string;
-    model: string;
-    additionalContext: string[];
-    localContext: string[];
-    groundTruths: string[];
-    botName: string;
-  }): Promise<ResponseFromLlm> {
+  async createCompletion(params: CreationParams, messages?: OpenAI.Chat.Completions.ChatCompletionMessageParam[]): Promise<ResponseFromLlm> {
+    const { model } = params;
     const res: OpenAI.Chat.Completions.ChatCompletion = await this.client.chat.completions.create({
+      // tools: LLM_TOOLS, might not be a good idea to have this available for the general chatbot
       model: model,
-      messages: [
-        {
-          role: "system",
-          content: [
-            {
-              type: "text",
-              text: this._createSystemMessage(systemMessage, additionalContext, localContext, groundTruths, botName),
-            },
-          ],
-        },
-        {
-          role: "user",
-          content: [
-            {
-              type: "text",
-              text: prompt,
-            },
-          ],
-        },
-      ],
+      messages: messages || appendToConversation(params),
       temperature: 0.2,
-      max_tokens: MAX_TOKENS,
+      // This value is now deprecated in favor of max_completion_tokens, and is not compatible with o1 series models.
+      // max_COMPLETION_tokens: MAX_COMPLETION_TOKENS,
+
+      /**An upper bound for the number of tokens that can be generated for a completion, including visible output tokens and reasoning tokens. */
+      max_completion_tokens: MAX_COMPLETION_TOKENS,
       top_p: 0.5,
       frequency_penalty: 0,
       presence_penalty: 0,
@@ -87,10 +35,96 @@ export class Completions extends SuperOpenAi {
         type: "text",
       },
     });
-    const answer = res.choices[0].message;
-    if (answer && answer.content && res.usage) {
-      return { answer: answer.content, tokenUsage: { input: res.usage.prompt_tokens, output: res.usage.completion_tokens, total: res.usage.total_tokens } };
+
+    await this.handleFunctionCalling(res, params);
+
+    return getAnswerAndTokenUsage(res);
+  }
+
+  async handleFunctionCalling(res: OpenAI.Chat.Completions.ChatCompletion, params: CreationParams) {
+    const { systemMessage, prompt, model, additionalContext, localContext, groundTruths, botName } = params;
+    if (res.choices[0].finish_reason === "function_call") {
+      const toolCalls = res.choices[0].message.tool_calls;
+      const choiceMessage = res.choices[0]["message"];
+
+      if (!toolCalls) {
+        return;
+      }
+
+      const fnCallResults: ToolCallResponse[] = [];
+
+      for (const toolCall of toolCalls) {
+        const { name, arguments: args } = toolCall.function;
+        let parsedArgs: { should_convert: boolean } = JSON.parse(args);
+
+        if (name === "convert_pull_request_to_draft") {
+          try {
+            parsedArgs = JSON.parse(args);
+          } catch (er) {
+            throw logger.error("Error parsing args for convert_pull_request_to_draft", {
+              args,
+              er,
+            });
+          }
+          let fnCallResponse;
+
+          if (!parsedArgs.should_convert) {
+            fnCallResponse = {
+              role: "tool",
+              content: "pull request meets the specification, no action taken.",
+              tool_call_id: toolCall.id,
+            };
+          } else {
+            let number;
+
+            if ("pull_request" in this.context.payload) {
+              number = this.context.payload.pull_request.number;
+            } else if ("issue" in this.context.payload) {
+              number = this.context.payload.issue.number;
+            }
+
+            if (!number) {
+              throw logger.error("No pull request or issue number found in payload");
+            }
+
+            await this.context.octokit.pulls.update({
+              owner: this.context.payload.repository.owner.login,
+              repo: this.context.payload.repository.name,
+              pull_number: number,
+              draft: true,
+            });
+
+            fnCallResponse = {
+              role: "tool",
+              content: "pull request did not meet the specification, converted to draft.",
+              tool_call_id: toolCall.id,
+            };
+          }
+
+          fnCallResults.push({
+            response: choiceMessage,
+            tool_call_response: {
+              content: fnCallResponse.content,
+              role: "tool",
+              tool_call_id: toolCall.id,
+            },
+          });
+        }
+      }
+      const newChat = appendToConversation(params, fnCallResults);
+
+      return await this.createCompletion(
+        {
+          systemMessage,
+          prompt,
+          model,
+          additionalContext,
+          localContext,
+          groundTruths,
+          botName,
+        },
+        newChat
+      );
     }
-    return { answer: "", tokenUsage: { input: 0, output: 0, total: 0 } };
   }
 }
